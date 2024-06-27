fbda0aaa8815bd89d2df636f66a8e6e3d6a07e04
mandelset <82321658+mandelset@users.noreply.github.com>
2024-06-27 07:33:26 +0200

    Modifiche varie

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const Home = {
     template: `
     <main>
         <article>
-            <h2>What does this website do?</h2>
-            <p>
+            <h2 id="h2-title" class="text-center m-4">What does this website do?</h2>
+			<!-- la classe img-fluid rende le immagini responsive -->
+           	<figure class="text-center"> 
+				<img src="../assets/emscripten_logo.png" class="figure-img img-fluid " style="width: 400px;" alt="Emscripten Logo"/>
+				<figcaption class="figure-caption">Emscripten, the compiler used to build the example of this website</figcaption>
+           	</figure> 
+            <p class="text-center m-4 p-2 bg-body-secondary">
                 This website utizilies technology able to convert C/C++ programs into web-assembly; This allows us to run such programs in the browser web. The use of script in web-assembly enables us to obtain better performance and optimized code, with this being very useful to calculation-focused applications (such as games and mathematical-oriented programs).
             </p>
-            <img src="../assets/emscripten_logo.png" alt="Photo of Ralph Baer"/>
-            <figcaption>Emscripten, the compiler used to build the example of this website</figcaption>
-            <img src="../assets/ClangWasm.png" alt="Emscripten Toolchain"/>
-            <figcaption>An example showing how simple the Emscripten toolchain could be</figcaption>
-        </article>
+           	<figure class="text-center"> 
+				<img src="../assets/ClangWasm.png" class="figure-img img-fluid" style="width: 500px;" alt="Emscripten Toolchain"/>
+            	<figcaption class="figure-caption">An example showing how simple the Emscripten toolchain could be</figcaption>
+       		</figure> 
+		</article>
     </main>
     `,
     mounted() {
@@ -22,28 +27,30 @@ const AboutES = {
     template: `
     <main>
         <article>
-            <h2>What is Emscripten?</h2>
-            <p>
+            <h2 id="h2-title" class="text-center m-4">What is Emscripten?</h2>
+            <p class="text-center m-4 p-2 bg-body-secondary">
                 Emscripten is a compiler that converts C and C++ code into WebAssembly (WASM) or JavaScript, allowing developers to run native code in web browsers or Node.js environments. Here's a brief history of Emscripten and an overview of what it does.
             </p>
-            <h3>Early days (2010-2011)</h3>
-            <p>
+            <h3 id="h3-title" class="text-left m-4">Early days (2010-2011)</h3>
+            <p class="text-center m-4 p-2 bg-body-secondary">
                 Emscripten was created by Alon Zakai, a Mozilla engineer, in 2010. At the time, Zakai was working on a project to port the Quake III Arena game engine to run in a web browser. He realized that compiling C code to JavaScript would be a more efficient approach than rewriting the entire engine in JavaScript. Zakai's initial implementation, called "Emscripten," was a simple C-to-JavaScript compiler.
             </p>
-
-            <img src="../assets/quake3.jpeg" alt="Quake 3 Arena screenshot"/>
-            <figcaption>A screenshot of id Software's game, Quake 3 Arena</figcaption>
-
-            <h3>Development and growth (2011-2013)</h3>
-            <p>
+			
+			<figure class="text-center">
+				<img src="../assets/quake3.jpeg" class="figure-img img-fluid" style="width: 500px;" alt="Quake 3 Arena screenshot"/>
+				<figcaption class="figure-caption">A screenshot of id Software's game, Quake 3 Arena</figcaption>
+			</figure>
+            
+			<h3 id="h3-title" class="text-right m-4">Development and growth (2011-2013)</h3>
+            <p class="text-center m-4 p-2 bg-body-secondary">
                 In 2011, Emscripten was open-sourced and gained popularity as a tool for porting native games and applications to the web. The compiler was initially based on the LLVM compiler infrastructure project, which provided a robust framework for building compilers. Emscripten's development accelerated, and it began to support more C and C++ features, including dynamic linking, exceptions, and RTTI (Run-Time Type Information).
             </p>
-            <h3>WebAssembly (2015)</h3>
-            <p>
+            <h3 id="h3-title" class="text-left m-4">WebAssembly (2015)</h3>
+            <p class="text-center m-4 p-2 bg-body-secondary">
                 In 2015, the WebAssembly (WASM) project was announced, aiming to provide a binary instruction format for the web. Emscripten was one of the first compilers to support WASM, and it became a key player in the WASM ecosystem. With WASM, Emscripten could generate more efficient and portable code, which could be executed by web browsers and Node.js environments without the need for JavaScript.
             </p>
-            <h3>Modern Emscripten (2016-present)</h3>
-            <div>
+            <h3 id="h3-title" class="text-right m-4">Modern Emscripten (2016-present)</h3>
+            <div class="text-center m-4 p-2 bg-body-secondary">
                 Today, Emscripten is a mature compiler that supports a wide range of C and C++ features, including:
                 <ul>
                     <li>WebAssembly (WASM) generation: Emscripten can compile C and C++ code to WASM, which is executed by web browsers and Node.js environments.</li>
@@ -53,8 +60,8 @@ const AboutES = {
                     <li>Emulation of native APIs: Emscripten provides emulations of native APIs, such as OpenGL, OpenAL, and SDL, allowing developers to port native applications with minimal modifications.</li>
                 </ul>
             </div>
-            <h3>Emscripten's Uses</h3>
-            <div>
+            <h3 id="h3-title" class="text-left m-4">Emscripten's Uses</h3>
+            <div class="text-center m-4 p-2 bg-body-secondary">
                 Emscripten is widely used in various industries, including:
                 <ul>
                     <li>Gaming: Emscripten has been used to port popular games like Quake III Arena, Doom, and Unreal Tournament to the web.</li>
@@ -62,9 +69,11 @@ const AboutES = {
                     <li>Enterprise software: Emscripten is used in enterprise software to port native applications to the web, reducing the need for native installations.</li>
                 </ul>
             </div>
-
-            <img src="../assets/screen1.jpg" alt="Emscripten app screenshot"/>
-            <figcaption>Unreal Engine 3 ported to Web, thanks to Emscripten</figcaption>
+			
+			<figure class="text-center">
+            	<img src="../assets/screen1.jpg" class="figure-img img-fluid" style="width: 500px;" alt="Emscripten app screenshot"/>
+            	<figcaption class="figure-caption">Unreal Engine 3 ported to Web, thanks to Emscripten</figcaption>
+			</figure>
 
         </article>
     </main>
